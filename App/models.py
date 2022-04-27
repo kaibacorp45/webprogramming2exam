6fd163e89ac8aa76e8941eafc3153c68b8a626cb
kaibacorp45 <>
2022-04-27 06:14:59 +0000

    lots of code

--- App/models.py
+++ App/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 db = SQLAlchemy()
 
@@ -12,3 +14,73 @@ def create_db(app):
     
 def init_db(app):
     db.init_app(app)
+
+# models
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username =  db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.set_password(password)
+
+    def toDict(self):
+        return{
+          'id': self.id,
+          'username': self.username
+        }
+
+    def set_password(self, password):
+        """Create hashed password."""
+        self.password = generate_password_hash(password, method='sha256')
+    
+    def check_password(self, password):
+        """Check hashed password."""
+        return check_password_hash(self.password, password)
+
+#review
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text =  db.Column(db.String(500), nullable=True)
+    rating = db.Column(db.Integer, nullable=True)
+    isbn = db.Column(db.String(120), db.ForeignKey('book.isbn'), nullable = False)
+
+    def toDict(self):
+        return{
+          'text': self.text,
+          'rating': self.rating
+      #add isbn maybe
+        }
+
+class Book(db.Model):
+    isbn = db.Column(db.String(120), primary_key=True)
+    title =  db.Column(db.String(120), nullable=True)
+    author = db.Column(db.String(120), nullable=True)
+    publication_year = db.Column(db.Integer, nullable = True)
+    publisher =  db.Column(db.String(120), nullable=True)
+    review = db.relationship('Review', backref='book', lazy=True,cascade="all, delete-orphan")
+
+    def toDict(self):
+        return{
+          'isbn': self.isbn,
+          'title': self.title,
+          'author': self.author,
+          'publication_year': self.publication_year,
+          'publisher': self.publisher,
+      #add isbn maybe
+        }
+
+    def get_avg_rating(self):
+      avg = 0
+      sum = 0
+      for r in review:
+        sum = sum + review.rating
+        count = count + 1
+      avg = sum/count
+      return avg
+
+
+   
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,61 @@
 import click
 from flask import Flask
 from App import create_db, db, app
-
+from App import Book
+import csv
 
 @app.cli.command("init")
 def initialize():
     create_db(app)
     print('database intialized')
+    book_list = []
+    with open('books.csv') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        isbnB = ""
+        book_title = ""
+        book_author = ""
+        year = ""
+        publisherN = ""
+        image_s = ""
+        image_m = ""
+        image_l = ""
+        
+        for line in csv_reader:
+            isbnB = line['ISBN']
+            if isbnB == "":
+                isbnB = None
+    
+            book_title = line['Book-Title']
+            if book_title =="":
+                 book_title = None
+            
+            book_author = line['Book-Author']
+            if book_author == "":
+                book_author = None
+    
+            year = line['Year-Of-Publication']
+            if year == "":
+                year = None
+    
+            publisherN = line['Publisher']
+            if publisherN =="":
+                publisherN = None
+            
+            image_s = line['Image-URL-S']
+            if image_s =="":
+                image_s = None
+    
+            image_m = line['Image-URL-M']
+            if image_m =="":
+                image_m = None
+    
+            image_l = line['Image-URL-L']
+            if image_l =="":
+                image_l = None
+    
+            book_list.append(Book(isbn = isbnB, title = book_title, author = book_author, publication_year = year, publisher = publisherN))
+
+        for b in book_list:
+          db.session.add(b)
+        db.session.commit()
+          
